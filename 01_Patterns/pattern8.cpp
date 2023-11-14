6f4db7b6363b91504a7c0b7f785e4c8b0aa653c9
Atharva <learnsocialacc.atharva@gmail.com>
2023-11-14 18:40:25 +0530

    till pattern8

---
+++ 01_Patterns/pattern8.cpp
@@ -0,0 +1,30 @@
+/*
+1
+2 3
+3 4 5
+4 5 6 7
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=1;
+    int count=1;
+    while(i<=n){
+        int j=1;
+        count=i;
+        while (j<=i)
+        {
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
